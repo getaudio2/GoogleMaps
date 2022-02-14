e0ff959413736e3a434bd58327a020c3574904b9
getaudio2 <paugermaisar@hotmail.es>
2022-02-14 19:44:50 +0100

    Created ApiThread class

---
+++ app/src/main/java/com/example/googlemaps/ApiThread.java
@@ -0,0 +1,65 @@
+package com.example.googlemaps;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ApiThread extends AsyncTask<Void, Void, String> {
+
+    private String latitude;
+    private String longitude;
+    private JSONObject jObject;
+
+    public ApiThread(String latitude, String longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        URL url = null;
+        try {
+            url = new URL("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+            // Read API results
+            InputStream inputStream = httpURLConnection.getInputStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String data = bufferedReader.readLine();
+
+            return data;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    @Override
+    protected void onPostExecute(String data){
+        super.onPostExecute(data);
+
+        try {
+            jObject = new JSONObject(data);
+            jObject = jObject.getJSONObject("results");
+            String sunrise = jObject.getString("sunrise");
+            Log.i("logtest", "------>" + sunrise);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
