a4b9caed8956236c82014c290214742f02a90287
getaudio2 <paugermaisar@hotmail.es>
2022-02-21 19:55:07 +0100

    Added PageView activity & SlidingAdapter

--- app/src/main/java/com/example/googlemaps/PageView.java
+++ app/src/main/java/com/example/googlemaps/PageView.java
@@ -1,14 +1,22 @@
 package com.example.googlemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class PageView extends AppCompatActivity {
+
+    ArrayList<String> urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_view);
+
+        ViewPager mPager = findViewById(R.id.vpager);
+        mPager.setAdapter(new SlidingAdapter(this, urls));
     }
 }
